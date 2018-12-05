a2e9138b8d93fdd1fe724a20a4d4dca355956118
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2018-12-04 17:15:09 -0800

    try shawn’s awesomesauce solution

--- ReactNativePlayground/appcenter-post-clone.sh
+++ ReactNativePlayground/appcenter-post-clone.sh
@@ -2,11 +2,13 @@
 
 node --version
 
-set -ex
-brew uninstall node@6
-NODE_VERSION="8.9.4"
-curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
-sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
-sudo npm cache clean -f
+# please specify required Node.js version
+NODE_VERSION=8.10.0
+
+# workaround to override the v8 alias
+npm config delete prefix
+. ~/.bashrc
+nvm install "$NODE_VERSION"
+nvm alias node8 "$NODE_VERSION"
 
 node --version
