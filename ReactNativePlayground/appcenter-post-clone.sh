3b59877a755e27a6b3ac752d955f6debcfe539ba
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2018-12-04 16:48:55 -0800

    Revert "try to use nvm"
    This reverts commit d5c0e10793606b5c7d7f1607ede4f6e23810ab99.

--- ReactNativePlayground/appcenter-post-clone.sh
+++ ReactNativePlayground/appcenter-post-clone.sh
@@ -4,11 +4,8 @@ node --version
 
 set -ex
 brew uninstall node@6
-# NODE_VERSION="8.9.4"
-# curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
-# sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
-
-nvm install 8.10.0
-nvm use 8.10.0
+NODE_VERSION="8.9.4"
+curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
+sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
 
 node --version
