40caf49d495e89db0547dfd8c443d382359f2f22
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2018-12-04 16:36:43 -0800

    try with 8.9.4

--- ReactNativePlayground/appcenter-post-clone.sh
+++ ReactNativePlayground/appcenter-post-clone.sh
@@ -4,7 +4,7 @@ node --version
 
 set -ex
 brew uninstall node@6
-NODE_VERSION="8.10.0"
+NODE_VERSION="8.9.4"
 curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
 sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
 
