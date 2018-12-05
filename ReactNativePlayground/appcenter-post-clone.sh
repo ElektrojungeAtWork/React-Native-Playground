dfaaedc4e7417754af4f62447f25174a6f8073ed
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2018-12-04 16:48:49 -0800

    Revert "moar"
    This reverts commit 26e9248aede6e4cc0b972fb46198f7d487d4742a.

--- ReactNativePlayground/appcenter-post-clone.sh
+++ ReactNativePlayground/appcenter-post-clone.sh
@@ -4,7 +4,6 @@ node --version
 
 set -ex
 brew uninstall node@6
-brew install nvm
 # NODE_VERSION="8.9.4"
 # curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
 # sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
